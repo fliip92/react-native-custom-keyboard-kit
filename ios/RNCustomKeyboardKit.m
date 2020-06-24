db430b66864bc19a8013d013684f5e12f573927a
fliip92 <jufelipe.la@gmail.com>
2020-06-24 11:07:58 -0700

    Update RNCustomKeyboardKit.m

--- ios/RNCustomKeyboardKit.m
+++ ios/RNCustomKeyboardKit.m
@@ -16,7 +16,7 @@
 
 RCT_EXPORT_MODULE(CustomKeyboardKit)
 
-RCT_EXPORT_METHOD(install:(nonnull NSNumber *)reactTag)
+RCT_EXPORT_METHOD(install:(nonnull NSNumber *)reactTag withType:(nonnull NSString *)keyboardType)
 {
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:@{@"reactTag": reactTag}];
   UIView* inputView = [[RCTRootView alloc] initWithBridge:_bridge
